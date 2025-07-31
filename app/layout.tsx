1969716f87b575540b315fee5e9f3b196bf0b0af
guilhermebetzila <guilhermeacessorialtda@gmail.com>
2025-07-31 12:24:28 -0300

    ajustes no layout e correção do CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
 // app/layout.tsx
-import './globals.css'
-import Providers from './Providers'
+import './globals.css';
+import Providers from './Providers';
 
 export const metadata = {
   title: 'Betdreams',
   description: 'App oficial Betdreams',
-}
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-br" data-scroll-behavior="smooth">
-      <body className="bg-gray-900 text-white min-h-screen flex overflow-y-auto">
+    <html lang="pt-br" data-scroll-behavior="smooth" className="h-full w-full">
+      <body className="bg-gray-900 text-white h-full w-full min-h-screen overflow-x-hidden">
         <Providers>
-          {children}
+          <div className="min-h-screen w-full flex flex-col">
+            {children}
+          </div>
         </Providers>
       </body>
     </html>
-  )
+  );
 }
