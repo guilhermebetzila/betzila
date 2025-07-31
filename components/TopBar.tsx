d4bfa8cc19bf584e79ab980b888bd451885a65d9
guilhermebetzila <guilhermeacessorialtda@gmail.com>
2025-07-31 10:05:16 -0300

    Remover botões de Entrar e Registrar-se do Topbar

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,20 +1,9 @@
 'use client';
 
-import Link from 'next/link';
-
 export function Topbar() {
   return (
-    <div className="fixed top-0 right-0 z-50 p-4 flex gap-4">
-      <Link href="/login">
-        <button className="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-1 px-4 rounded">
-          Entrar
-        </button>
-      </Link>
-      <Link href="/register">
-        <button className="bg-white hover:bg-gray-200 text-black font-bold py-1 px-4 rounded border">
-          Registrar-se
-        </button>
-      </Link>
+    <div className="fixed top-0 right-0 z-50 p-4">
+      {/* Botões removidos */}
     </div>
   );
 }
