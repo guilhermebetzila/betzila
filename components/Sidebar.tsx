72ba81031f1263af82421abdf98e9d2bf0b08502
guilhermebetzila <guilhermeacessorialtda@gmail.com>
2025-08-12 11:39:05 -0300

    ajuste no import do Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,31 +11,37 @@ import {
 
 export function Sidebar() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-gray-900 text-white flex justify-around items-center h-16 z-50 border-t border-gray-800 md:hidden">
-      <Link href="/dashboard">
-        <SidebarItem icon={<FaChartLine />} label="Painel" />
-      </Link>
-      <Link href="/games/ia">
-        <SidebarItem icon={<FaRocket />} label="IA" />
-      </Link>
-      <Link href="/games/investir">
-        <SidebarItem icon={<FaWallet />} label="Investir" />
-      </Link>
-      <Link href="/como-funciona">
-        <SidebarItem icon={<FaBrain />} label="Como Funciona" />
-      </Link>
-      <Link href="/ecossistema">
-        <SidebarItem icon={<FaGlobe />} label="Ecosistema" />
-      </Link>
+    <nav
+      className="fixed bottom-0 left-0 right-0 bg-gray-900 text-white flex justify-around items-center h-16 z-50 border-t border-gray-800 md:hidden shadow-lg"
+      role="navigation"
+      aria-label="Menu inferior"
+    >
+      <SidebarItem href="/dashboard" icon={<FaChartLine />} label="Painel" />
+      <SidebarItem href="/games/ia" icon={<FaRocket />} label="IA" />
+      <SidebarItem href="/games/investir" icon={<FaWallet />} label="Investir" />
+      <SidebarItem href="/como-funciona" icon={<FaBrain />} label="Como Funciona" />
+      <SidebarItem href="/ecossistema" icon={<FaGlobe />} label="Ecossistema" />
     </nav>
   );
 }
 
-function SidebarItem({ icon, label }: { icon: React.ReactNode; label: string }) {
+function SidebarItem({
+  href,
+  icon,
+  label
+}: {
+  href: string;
+  icon: React.ReactNode;
+  label: string;
+}) {
   return (
-    <div className="flex flex-col items-center justify-center text-xs hover:text-yellow-400 cursor-pointer">
+    <Link
+      href={href}
+      className="flex flex-col items-center justify-center text-xs hover:text-yellow-400 transition-colors duration-200"
+    >
       <div className="text-lg">{icon}</div>
       <span>{label}</span>
-    </div>
+    </Link>
   );
 }
+
